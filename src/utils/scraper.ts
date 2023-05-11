5a129cd9a301331747660dc6b7b5446cc76f9085
Trevron <trevrontv@gmail.com>
2023-05-11 22:03:11 +0200

    Case insensitive sorting

--- src/utils/scraper.ts
+++ src/utils/scraper.ts
@@ -55,7 +55,20 @@ export async function scrapeUsernames(id: string, pageCount: number): Promise<st
     return array.indexOf(value) === index;
   }
 
-  return usernames.filter(onlyUnique).sort();
+  const caseInsensitiveCompare = (a: string, b: string): number => {
+    const lowerA = a.toLowerCase();
+    const lowerB = b.toLowerCase();
+  
+    if (lowerA < lowerB) {
+      return -1;
+    }
+    if (lowerA > lowerB) {
+      return 1;
+    }
+    return 0;
+  };
+
+  return usernames.filter(onlyUnique).sort(caseInsensitiveCompare);
 }
 
 export async function getPost(id: string): Promise<string> {
